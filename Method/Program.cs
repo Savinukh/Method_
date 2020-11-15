a524a08ad4136af5f9040662fc066010d7bb2667
Left_Tapochek <serregasavinykh15@gmail.com>
2020-11-16 01:44:25 +0500

    Добавление метода: печати желтой линии (кол-во заданное пользователем)

--- Method/Program.cs
+++ Method/Program.cs
@@ -8,9 +8,11 @@ namespace Method
         {
             MethodRedLine();
             MethodSquare();
+            MethodYellowLine();
+
         }
         static void MethodSquare()
-        {
+       {
             for (int i = 1; i <= 10; i++)
             {
                 for (int n = 1; n <= 10; n++)
@@ -25,13 +27,39 @@ namespace Method
         }
         static void MethodRedLine()
         {
-            Console.WriteLine("Введите длины красной линии:\t");
+            Console.Write("Введите длину красной линии:\t");
             int length = int.Parse(Console.ReadLine());
             for (int i = 1; i <= length; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("─");
+                Console.ResetColor();
+            }
+            Console.WriteLine();
+            Console.ReadKey();
         
        
 
 
     }
+        static void MethodYellowLine()
+        {
+            Console.Write("Введите длину линии:\t");
+            int length = int.Parse(Console.ReadLine());
+            Console.Write("Введите количество линий:\t");
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= count; i++)
+            {
+                for (int n = 1; n <= length; n++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write("─");
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+            MethodSquare();
+        } 
 }
 }
